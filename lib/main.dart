028a2a07e2fdaa8336cf69b9f3cd8986505c8fa2
MajorFrieza <onlinetoram212@gmail.com>
2025-12-24 20:00:13 +0800

    Frontend loginpage & staff almost done. Just need a few enhancement for code stability

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 import 'screens/staff/staff_home_screen.dart';
 
 void main() {
@@ -21,57 +22,6 @@ class MAMSApp extends StatelessWidget {
         '/staffHome': (context) => StaffHomeScreen(),
         '/adminHome': (context) => AdminHomeScreen(),
       },
-    );
-  }
-}
-
-// Login Screen
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  String role = 'Staff'; // Default toggle
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ToggleButtons(
-              children: [Text('Staff'), Text('Admin')],
-              isSelected: [role == 'Staff', role == 'Admin'],
-              onPressed: (index) {
-                setState(() {
-                  role = index == 0 ? 'Staff' : 'Admin';
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            TextField(decoration: InputDecoration(labelText: 'Username')),
-            TextField(
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              child: Text('Login'),
-              onPressed: () {
-                if (role == 'Staff') {
-                  Navigator.pushReplacementNamed(context, '/staffHome');
-                } else {
-                  Navigator.pushReplacementNamed(context, '/adminHome');
-                }
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
